3e66d471cf48553df2f640accbadacc3b0557f72
ST10303285 <ST10303285@vcconnect.edu.za>
2024-04-02 23:17:39 +0200

    Add project files.

---
+++ Models/SQLExample1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Models
+{
+    public class SQLExample1 : Controller
+    {
+        public static string con_string = "Server=tcp:webserver-example1.database.windows.net,1433;Initial Catalog=database1;Persist Security Info=False;User ID=wadihaboat;Password=Malika2003.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_string);
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
